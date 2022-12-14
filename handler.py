3e6012f5f59e57e174f00719e8471b95b9c64e5e
jit-shlomi <shlomi@jit.io>
2022-12-14 07:58:31 +0200

    write our handlers

--- handler.py
+++ handler.py
@@ -1,21 +1,47 @@
 import json
 
+import boto3
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
-    }
+USERS_TABLE = "Users"
+dynamodb_client = boto3.client(
+    'dynamodb', region_name='us-east-1', endpoint_url='http://localstack:4566'
+)
 
+
+def _verify_response(response):
+    if "Error" in response:
+        print(response)
+        raise Exception(f"HTTPStatusCode: {response['ResponseMetadata']['HTTPStatusCode']}"
+                        f"message={response['Error']['Message']}")
+
+
+def get_user(event, context):
+    user_id = event['pathParameters']["user_id"]
+    result = dynamodb_client.get_item(
+        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
+    )
+    _verify_response(result)
+    item = result.get('Item')
+
+    if not item:
+        return {"statusCode": 404, "body": json.dumps({})}
+    body = {'user_id': item.get('userId').get('S'), 'name': item.get('name').get('S')}
     response = {"statusCode": 200, "body": json.dumps(body)}
-
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+
+def create_user(event, context):
+    print(f"{event=}")
+    request = json.loads(event["body"])
+    user_id = request["user_id"]
+    user_name = request["user_name"]
+    result = dynamodb_client.put_item(
+        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': user_name}}
+    )
+    _verify_response(result)
+
+    body = {
+        "message": "Successfuly added a new user!"
     }
-    """
+    response = {"statusCode": 201, "body": json.dumps(body)}
+    return response
